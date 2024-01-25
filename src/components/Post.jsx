658da930128a6653f793eac3dbeac8d833c401dd
vbuarque <viniciusbuarque17@gmail.com>
2024-01-25 15:04:17 -0300

    feat: criando o componente de Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,26 @@
-export function Post(props) {
+import styles from './Post.module.css'
+
+export function Post() {
     return (
-        <div>
-            <strong>{props.author}</strong>
-            <p>{props.content}</p>
-        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/vbuarque.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Vinicius Buarque</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='13 de Janeiro às 08:13h' dateTime="2024-01-13 08:13:30">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
     )
 }
