8fbd48b44b347d09b9bd062b83116d9b1cdbfaae
vbuarque <viniciusbuarque17@gmail.com>
2024-01-25 00:42:19 -0300

    style: finalizando a estilização do componente sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilSimpleLine } from '@phosphor-icons/react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +10,17 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/vbuarque.png" />
+
                 <strong>Vinicius Buarque</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilSimpleLine
+                        size={20}
+                    />
                     Editar seu perfil
                 </a>
             </footer>
